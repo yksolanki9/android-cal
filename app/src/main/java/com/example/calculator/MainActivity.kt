146d04c97b507709fce79f1ec188f96505cd9bd3
Yash Solanki <yashsolankli1709@gmail.com>
2022-06-05 19:48:18 +0530

    Build app ui, show entered values in TextView and add clear input operation

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tv_calc)
+    }
+
+    fun onDigitClicked(view: View) {
+        tvInput?.append((view as Button).text)
+    }
+
+    fun clearInput(view: View) {
+        tvInput?.text = ""
     }
 }
