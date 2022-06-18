b87e1d7e4654c464a5c04782965fde9cb8005e3f
yksolanki9 <yashsolankli1709@gmail.com>
2022-06-18 16:48:15 +0530

    Build app ui, show entered values in TextView and add clear input operation

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +23,54 @@ class MainActivity : AppCompatActivity() {
         tvInput?.append((view as Button).text)
     }
 
+    fun onOperatorClicked(view: View) {
+        tvInput?.text?.let {
+            val inputStr = it.toString()
+            if(inputStr.isEmpty() && view.id == R.id.btn_sub)
+                tvInput?.append((view as Button).text)
+            else if(inputStr.isNotEmpty())
+                tvInput?.append((view as Button).text)
+            else
+                tvInput
+        }
+    }
+
+    fun onPeriodClicked(view: View) {
+        //Handle cases where more than one period should not be allowed in a single operand
+    }
+
+    // Need to create a mapping from operator unicode to actual operator instead of using unicode
+    // everytime to compare strings
+    fun calculate(view: View) {
+        tvInput?.text?.let {
+            try {
+                val inputStr = it.toString()
+                Log.d("TEST", inputStr)
+                Log.d("CHAR", inputStr.contains('\u2212').toString())
+
+                if(inputStr.contains('\u2212')) {
+                    val operands = inputStr.split('\u2212')
+                    Log.d("TEST", operands.toString())
+                    tvInput?.text = (operands[0].toDouble() - operands[1].toDouble()).toString()
+                }
+
+            } catch(e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
     fun clearInput(view: View) {
         tvInput?.text = ""
+    }
+
+    fun clearLastChar(view: View) {
+        tvInput?.text?.let {
+            val inputStr = it.toString()
+            if(inputStr.isNotEmpty()) {
+                tvInput?.text = inputStr.substring(0, inputStr.length - 1)
+            }
+
+        }
     }
 }
