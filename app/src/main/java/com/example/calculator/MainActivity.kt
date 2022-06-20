f7ae454e64adcc73211f18457e34124c310f91e7
yksolanki9 <yashsolankli1709@gmail.com>
2022-06-20 23:56:52 +0530

    Fix logic to do all calculations

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -11,12 +11,25 @@ import java.lang.ArithmeticException
 class MainActivity : AppCompatActivity() {
 
     private var tvInput: TextView? = null
+    private var tvRes: TextView? = null
+    private var btnDiv: Button? = null
+
+    private val operators = mapOf<String, Char>("MOD" to '\u0025', "DIV" to '\u00f7', "MUL" to '\u00d7', "SUB" to '\u2212', "ADD" to '\u002b')
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tvInput = findViewById(R.id.tv_calc)
+        tvRes = findViewById(R.id.tv_res)
+
+        btnDiv = findViewById(R.id.btn_div)
+
+
+        btnDiv?.setOnClickListener {
+
+        }
     }
 
     fun onDigitClicked(view: View) {
@@ -48,10 +61,11 @@ class MainActivity : AppCompatActivity() {
                 Log.d("TEST", inputStr)
                 Log.d("CHAR", inputStr.contains('\u2212').toString())
 
-                if(inputStr.contains('\u2212')) {
-                    val operands = inputStr.split('\u2212')
-                    Log.d("TEST", operands.toString())
-                    tvInput?.text = (operands[0].toDouble() - operands[1].toDouble()).toString()
+                for(entry in operators.entries) {
+                    if(inputStr.contains(entry.value)) {
+                        tvInput?.text = calculateUtil(inputStr, entry.key).toString()
+                        break
+                    }
                 }
 
             } catch(e: ArithmeticException) {
@@ -70,7 +84,62 @@ class MainActivity : AppCompatActivity() {
             if(inputStr.isNotEmpty()) {
                 tvInput?.text = inputStr.substring(0, inputStr.length - 1)
             }
-
         }
     }
+
+    private fun calculateUtil(inputStr: String, operator: String): Double {
+        val operands = operators[operator]?.let { inputStr.split(it) }
+        val operand1 = operands?.get(0)?.toDouble()
+        val operand2 = operands?.get(1)?.toDouble()
+
+        val operatorUni = operators[operator]
+        var res: Double? = null;
+
+        operand1?.let {
+            operand2?.let {
+                return when(operator) {
+                    "MOD" -> operand1 % operand2
+                    "DIV" -> operand1 / operand2
+                    "MUL" -> operand1 * operand2
+                    "SUB" -> operand1 - operand2
+                    "ADD" -> operand1 + operand2
+                    else -> 0.0
+                }
+            }
+        }
+        return 0.0
+    }
+
+//    fun getOperator(operatorUni: String) {
+//        when (operatorUni) {
+//            "\u0025" -> "MOD"
+//            "\u00f7" -> "DIV"
+//            "\u00d7" -> "MUL"
+//            "\u2212" -> "SUB"
+//            "\u002b" -> "ADD"
+//        }
+//    }
+//    "MOD" of '\u0025', "DIV" of '\u00f7', "MUL" of '\u00d7', "SUB" of '\u2212', "ADD" of '\u002b'
+//
+//    fun getOperatorUni(operator: String) {
+//        when (operator) {
+//            "MOD" -> '\u0025'
+//            "DIV" -> '\u00f7'
+//            "MUL" -> '\u00d7'
+//            "SUB" -> '\u2212'
+//            "ADD" -> '\u002b'
+//        }
+//    }
+
+//    if(operator == "MOD") {
+//        res = operand1 % operand2
+//    } else if(operator == "DIV") {
+//        res = operand1 / operand2
+//    } else if(operator == "MUL") {
+//        res = operand1 * operand2
+//    } else if(operator == "SUB") {
+//        res = operand1 - operand2
+//    } else if(operator == "ADD") {
+//        res = operand1 + operand2
+//    }
 }
